ba3d03795883730ac90e6fa3aef7096398988504
SamuelTownsend <samjaytee13@gmail.com>
2022-06-06 09:57:27 -0700

    corrections

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
-"""
-module for filesotrage init
-"""
-# from models.engine.file_storage import FileStorage
-# from models.base_model import Basemodel
-# storage = Filestorage()
-# storage.reload()
+from models.engine.file_storage import FileStorage
+from models.base_model import Basemodel
+ 
+ storage = Filestorage()
+ storage.reload()
