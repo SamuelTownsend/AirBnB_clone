ae7c2f167bcd51cd84bf74afc21e7cbd51bbc0b5
SamuelTownsend <samjaytee13@gmail.com>
2022-06-06 09:18:38 -0700

    corrections

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ Module contains the Superclass: BaseModel
 """
 from datetime import datetime 
 import uuid
+from models import storage
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
@@ -31,7 +32,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-
+            storage.new(self)
 
 
 
@@ -46,6 +47,7 @@ class BaseModel:
         updates the public instance attribute with the current datetime
         """
         self.updated_at = datetime.now()
+        storage.save()
 
 
     def to_dict(self):
