4e4e635add39e655c3a81c348e6ec4212e6690bc
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-06-07 14:27:29 -0500

    Add files via upload

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-#from models.engine.file_storage import FileStorage
-#from models.base_model import Basemodel
+from models.engine.file_storage import FileStorage
+
  
-#storage = FileStorage()
-#storage.reload()
+storage = FileStorage()
+storage.reload()
