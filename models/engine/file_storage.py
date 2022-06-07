bc585cd46f2ef8b02ffef3fa4fceac7442894272
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-06-07 14:12:47 -0500

    Add files via upload

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,31 +10,31 @@ from models.amenity import Amenity
 from models.review import Review
 
 
-class self:
+class FileStorage:
     """ new class for storage"""
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
         """returns the dictionary of objects"""
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """sets in the obj with the key"""
         nameof = obj.__class__.__name__
-        self.__objects["{}.{}".format(nameof, obj.id)] = obj
+        FileStorage.__objects["{}.{}".format(nameof, obj.id)] = obj
 
     def save(self):
         """serializes the object to the json file"""
-        odict = self.__objects
+        odict = FileStorage.__objects
         objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
-        with open(self.__file_path, "w+") as f:
+        with open(FileStorage.__file_path, "w+") as f:
             json.dump(objdict, f)
 
     def reload(self):
         """deserializes the JSON file to the object"""
         try:
-            with open(self.__file_path) as f:
+            with open(FileStorage.__file_path) as f:
                 objdict = json.load(f)
                 for o in objdict.values():
                     cls_name = o["__class__"]
