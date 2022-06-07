29d6ec6bbe215021ee69766f660f735e5c6e9667
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-06-07 14:27:10 -0500

    Add files via upload

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ Module contains the Superclass: BaseModel
 """
 from datetime import datetime
 import uuid
-
+from models import storage
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
@@ -32,7 +32,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-
+            storage.new(self)
 
     def __str__(self):
         """
@@ -46,6 +46,7 @@ class BaseModel:
         updates the public instance attribute with the current datetime
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
