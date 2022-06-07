447a2b6c427ff3442008ab0400c1d88adfa80ba9
SamuelTownsend <samjaytee13@gmail.com>
2022-06-06 21:00:16 -0700

    commit for console

---
+++ console.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+this module contains the command interpreter
+"""
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """
+    Class is for a command line interpreter
+    """
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Uses quit as a command to exit"""
+        return True
+
+    def do_EOF(self, arg):
+        """command for end of file"""
+        return True
+
+    def emptyline(self):
+        """empty line"""
+        return 
+
+    def do_create(self, *arg)
