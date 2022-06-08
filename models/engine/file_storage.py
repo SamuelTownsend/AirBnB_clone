7f56f610f34b934356780e5ab854fe387b9e9fe1
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-06-07 22:44:36 -0500

    Add files via upload

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,29 +1,16 @@
 #!/usr/bin/python3
 """heading for py file"""
+
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
-def classes(self):
-        """Method for importing classes to fix circular import"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.place import Place
-        from models.review import Review
-
-        classes = {
-            "BaseModel": BaseModel,
-            "User": User,
-            "State": State,
-            "City": City,
-            "Amenity": Amenity,
-            "Place": Place,
-            "Review": Review
-            }
-        return classes
-
-
+        
 class FileStorage:
     """ new class for storage"""
     __file_path = "file.json"
@@ -31,28 +18,37 @@ class FileStorage:
 
     def all(self):
         """returns the dictionary of objects"""
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
         """sets in the obj with the key"""
         nameof = obj.__class__.__name__
-        FileStorage.__objects["{}.{}".format(nameof, obj.id)] = obj
+        nameof = nameof + "." + obj.id
+        self.__objects.setdefault(nameof, obj)
 
     def save(self):
         """serializes the object to the json file"""
-        odict = FileStorage.__objects
-        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
-        with open(FileStorage.__file_path, "w+") as f:
-            json.dump(objdict, f)
+        odict = {}
+        for x in self.__objects: 
+            odict.setdefault(x, self.__objects[x].to_dict())
+        jsdic = json.dumps(odict)
+        with open(self.__file_path, "w") as f:
+            f.write(jsdic)
 
     def reload(self):
         """deserializes the JSON file to the object"""
+        class_list = [BaseModel, User, State, Amenity, Place, City, Review]
         try:
-            with open(FileStorage.__file_path) as f:
-                objdict = json.load(f)
-                for o in objdict.values():
-                    cls_name = o["__class__"]
-                    del o["__class__"]
-                    self.new(eval(cls_name)(**o))
-        except FileNotFoundError:
-            return
+            odict = {}
+            odic2 = {}
+            with open(self.__file_path, "r") as f:
+                odict = json.load(f)
+            for key in odict:
+                y = odict[key]["__class__"]
+                for idx, item in enumerate(class_list):
+                    if y in str(item):
+                        a = class_list[idx](**odict[key])
+                odic2.setdefault(key, a)
+            self.__objects = odic2
+        except:
+            pass
